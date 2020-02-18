019dbf09f0a00f673a5537795a2c74fbc4520ed3
Marcin Koziej <marcin@cahoots.pl>
2020-02-18 09:44:12 +0100

    Document input types

--- lib/proca_web/schema.ex
+++ lib/proca_web/schema.ex
@@ -8,16 +8,21 @@ defmodule ProcaWeb.Schema do
   query do
     @desc "Get a list of campains"
     field :campaigns, list_of(:campaign) do
+      @desc "Filter campaigns by title using LIKE format (% means any sequence of characters)"
       arg :title, :string
+      @desc "Filter campaigns by name (exact match). If found, returns list of 1 campaign, otherwise an empty list"
       arg :name, :string
+      @desc "Filter campaigns by id. If found, returns list of 1 campaign, otherwise an empty list"
       arg :id, :integer
       resolve &Resolvers.Campaign.list/3
     end
 
     @desc "Get action page"
     field :action_page, :action_page do
-      arg :url, :string
+      @desc "Get action page by id."
       arg :id, :integer
+      @desc "Get action page by url the widget is displayed on"
+      arg :url, :string
       resolve &Resolvers.ActionPage.find/3
     end
   end
@@ -25,7 +30,9 @@ defmodule ProcaWeb.Schema do
   mutation do
     @desc "Adds a signature"
     field :add_signature, type: :id do
+      @desc "ID of ActionPage of widget adding signature"
       arg :action_page_id, non_null(:id)
+      @desc "Signature data"
       arg :signature, :signature_input
 
       resolve &Resolvers.Contact.add_signature/3
--- lib/proca_web/schema/input_types.ex
+++ lib/proca_web/schema/input_types.ex
@@ -1,30 +1,46 @@
 defmodule ProcaWeb.Schema.InputTypes do
   use Absinthe.Schema.Notation
 
+  @desc "Type to describe an area (identified by area_code) in some administrative division (area_type). Area code can be an official code or just a name, provided they are unique."
   input_object :area_input do
     field :area_code, :string
     field :area_type, :string
   end
 
+  @desc "Address type which can hold different addres fields."
   input_object :address_input do
+    @desc "Country code (two-letter)."
     field :country, :string
+    @desc "Postcode, in format correct for country locale"
     field :postcode, :string
+    @desc "List of areas this address belongs to"
     field :areas, list_of(:area_input)
 
     field :latitute, :float
     field :longitute, :float
   end
 
+  @desc "Custom field with a key and value. Both are strings."
   input_object :custom_field_input do
     field :key, :string
     field :value, :string
   end
 
+  @desc "Petition signature data. Only name is mandatory.
+Email and phone will be checked for format.
+For address, you can pass none or many different fields that specify it.
+All other fields should come in custom fields as key-value pairs.
+"
   input_object :signature_input do
+    @desc "Contacts name, first name first"
     field :name, :string
+    @desc "Contacts email address"
     field :email, :string
+    @desc "Contacts phone number"
     field :phone, :string
+    @desc "Contacts address"
     field :address, :address_input
+    @desc "Other fields that accompany the signature"
     field :custom_fields, list_of(:custom_field_input)
   end
 end
