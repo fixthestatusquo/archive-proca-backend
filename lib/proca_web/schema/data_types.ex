500133378c02634fc8d16880685f350e734c2bbe
Marcin Koziej <marcin@cahoots.pl>
2020-02-18 09:45:40 +0100

    Add data types doc

--- lib/proca_web/schema/data_types.ex
+++ lib/proca_web/schema/data_types.ex
@@ -3,14 +3,19 @@ defmodule ProcaWeb.Schema.DataTypes do
 
   object :campaign do
     field :id, :id
+    @desc "Internal name of the campaign"
     field :name, :string
+    @desc "Full, official name of the campaign"
     field :title, :string
   end
 
   object :action_page do
     field :id, :id
+    @desc "Locale for the widget, in i18n format"
     field :locale, :string
+    @desc "Url where the widget is hosted"
     field :url, :string
+    @desc "Campaign this widget belongs to"
     field :campaign, :campaign
   end
 end
