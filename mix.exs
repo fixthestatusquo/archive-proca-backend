e06665e7c8606cb549ba4eec32a7f2e0c6418646
Marcin Koziej <marcin@cahoots.pl>
2020-07-21 22:58:10 +0200

    Configure release to produce a tar file

--- mix.exs
+++ mix.exs
@@ -11,7 +11,12 @@ defmodule Proca.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      releases: [
+        prod: [
+          steps: [:assemble, :tar]
+        ]
+      ]
     ]
   end
 
