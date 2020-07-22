328298b54d68c8d068f55f57625c0c333dcd5fcd
Marcin Koziej <marcin@cahoots.pl>
2020-07-22 09:24:09 +0200

    Configure mix_systemd

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Proca.MixProject do
       {:pow, "~> 1.0.20"},
       {:proper_case, "~> 1.0.2"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:mix_systemd, "~> 0.7.3", runtime: false},
+      {:mix_systemd, "~> 0.7.3"},
       {:floki, ">= 0.0.0", only: :test},
       {:ex_machina, "~> 2.4", only: :test}
     ]
