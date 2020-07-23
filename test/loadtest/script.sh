cfd38804d8842ce7922db593155572e4b59af0da
xavier.dutoit <xavier@tttp.eu>
2020-07-23 10:23:34 +0200

    with payload adapted to the new API

--- test/loadtest/script.sh
+++ test/loadtest/script.sh
@@ -1,4 +1,4 @@
 # good old ab, trying to add 1000 signatures from 500 concurrent users. Will the server drop a ball and 5xx a request?
 # checkout output.txt for an example (hint, no, it doesn't)
-ab -n 1000 -c 500 -p payload.json -T application/json https://api.proca.foundation/api
+ab -n 1000 -c 500 -p payload.json -T application/json https://api.proca.app/api
 
