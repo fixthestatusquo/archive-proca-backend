02dfe01026580101d7fa593f90c66e21f7cc5e54
Marcin Koziej <marcin@cahoots.pl>
2020-07-27 15:51:06 +0200

    Fix migration to set org_id (now added)

--- priv/repo/migrations/20200727095935_add_org_id_to_contacts.exs
+++ priv/repo/migrations/20200727095935_add_org_id_to_contacts.exs
@@ -4,6 +4,20 @@ defmodule Proca.Repo.Migrations.AddOrgIdToContacts do
   def change do
     alter table(:contacts) do
       add :org_id, references(:orgs, on_delete: :nilify_all)
+
+      execute """
+      UPDATE contacts
+      SET org_id = pk.org_id
+      FROM public_keys pk
+      WHERE contacts.public_key_id = pk.id AND contacts.org_id is null
+      """
+
+      execute """
+      UPDATE contacts
+      SET org_id = ap.org_id
+      FROM supporters sup, action_pages ap
+      WHERE sup.id = contacts.supporter_id AND ap.id = sup.action_page_id AND contacts.org_id is null
+      """
     end
   end
 end
