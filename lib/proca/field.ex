d09ff78617ef9f4da2e8e1d2b46e81e7894c9303
Marcin Koziej <marcin@cahoots.pl>
2020-08-26 14:19:34 +0200

    Fix Field validation wrt empty value

--- lib/proca/field.ex
+++ lib/proca/field.ex
@@ -19,11 +19,11 @@ defmodule Proca.Field do
 
   def changeset(attr = %{key: _key, value: _value}) do
     %Field{}
-    |> cast(attr, [:key, :value, :transient])
-    |> validate_required([:key, :value])
+    |> cast(attr, [:key, :value, :transient], empty_values: [])
+    # |> validate_required([:key, :value])
     |> validate_format(:key, ~r/^([\w\d_-]+$)/)
     |> validate_length(:key, min: 1, max: 64)
-    |> validate_length(:value, min: 1, max: 1024)
+    |> validate_length(:value, max: 1024)
   end
 
   @doc """
