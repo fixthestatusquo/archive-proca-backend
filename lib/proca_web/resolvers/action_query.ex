82bea1ec7a4837ed6ae4629b4e6b56195356143c
Marcin Koziej <marcin@cahoots.pl>
2020-12-16 17:36:33 +0100

    Fix public actions limit

--- lib/proca_web/resolvers/action_query.ex
+++ lib/proca_web/resolvers/action_query.ex
@@ -6,26 +6,34 @@ defmodule ProcaWeb.Resolvers.ActionQuery do
   alias Proca.Repo
   alias Proca.Action
 
-  def actions_for_campaign(campaign_id) do
-    from(a in Action,
-      join: c in assoc(a, :campaign),
-      join: f in assoc(a, :fields),
-      where: c.id == ^campaign_id,
-      where: fragment("? || ':' || ?", a.action_type, f.key) in c.public_actions,
-      preload: [:fields]
-    )
-  end
-
   @max_list_size 100
-  def list_by_action_type(%{id: campaign_id}, params = %{action_type: action_type}, _ctx) do
+  def list_by_action_type(%{id: campaign_id, public_actions: public_actions}, params = %{action_type: action_type}, _ctx) do
     limit = min(params.limit, @max_list_size)
 
-    query =  actions_for_campaign(campaign_id)
+
+    select_actions = from(a in Action,
+      join: c in assoc(a, :campaign),
+      where: c.id == ^campaign_id and a.action_type == ^action_type and a.processing_status in [:accepted, :delivered],
+      order_by: [desc: :inserted_at],
+      limit: ^limit,
+      select: a.id
+    )
+
+    query = from(a in Action,
+      join: f in assoc(a, :fields),
+      where: a.id in subquery(select_actions),
+      where: fragment("? || ':' || ?", a.action_type, f.key) in ^public_actions,
+      preload: [fields: f]
+    )
+
+    # from(a in Action,
+    #   join: c in assoc(a, :campaign),
+    #   join: f in assoc(a, :fields),
+    #   where: c.id == ^campaign_id,
+    #   where: fragment("? || ':' || ?", a.action_type, f.key) in c.public_actions,
+    #   preload: [:fields]
+
     list = query
-    |> where([a, c], a.action_type == ^action_type and a.processing_status in [:accepted, :delivered])
-    |> order_by([a, c], desc: a.inserted_at)
-    |> limit([a, c], ^limit)
-    |> select([a, c], a)
     |> Repo.all()
     |> Enum.map(fn a -> %{
                         action_id: a.id,
