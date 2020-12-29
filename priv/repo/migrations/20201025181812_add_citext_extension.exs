87735c0a3f94b1be213f6feadd6f4513aef87e9d
Marcin Koziej <marcin@cahoots.pl>
2020-12-29 17:25:15 +0100

    Add if exists to creating of citext

--- priv/repo/migrations/20201025181812_add_citext_extension.exs
+++ priv/repo/migrations/20201025181812_add_citext_extension.exs
@@ -2,7 +2,7 @@ defmodule Proca.Repo.Migrations.AddCitextExtension do
   use Ecto.Migration
 
   def up do
-    execute "CREATE EXTENSION citext"
+    execute "CREATE EXTENSION IF NOT EXISTS citext"
   end
 
   def down do
